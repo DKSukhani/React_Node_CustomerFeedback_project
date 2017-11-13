6487fee02659b978e45f84cc5f4c32ee3dc3aeb3
DKSukhani <7866337+DKSukhani@users.noreply.github.com>
2017-11-13 18:29:20 +0800

    refactoring of the code by taking out the routes file and also the authRoutes file

--- server/index.js
+++ server/index.js
@@ -1,30 +1,7 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./services/passport");
 const app = express();
-const keys = require("./config/keys");
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken is ", accessToken);
-      console.log("refreshToken is ", refreshToken);
-      console.log("profile is ", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log("Server has started on PORT" + PORT));
