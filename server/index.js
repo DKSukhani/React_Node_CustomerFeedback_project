3726f945b9d6e378fc4a6b0608a4f8e9a515e8ab
Dipesh Sukhani <dipeshsukhani@gmail.com>
2017-11-30 05:22:56 +0800

    UPDATE
    UPDATE

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const keys = require('./config/keys.js')
+require ("./models/user.js");
 require("./services/passport");
 
 
--- server/services/passport.js
+++ server/services/passport.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
+const User = mongoose.model('users');
+
 passport.use(
   new GoogleStrategy(
     {
